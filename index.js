8c0836621fa7c5c4779a038af572dae8f8797b7d
Fabiola De La Torre <cubias.fabiola@gmail.com>
2023-03-06 03:14:51 -0600

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+return gifts;
+}
+wrapGifts(gifts);
+
+
+function writeCards(names, event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+        debugger;
+    }
+    return messages;
+}
+writeCards(["Sam", "Pat", "Gus"], "birthday");
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown-- );
+    }
+}
